a84c7bd6c30c84b414cc01063069a4ad3610c4bd
alexustinofff <a.ustinov0011@mail.ru>
2021-06-23 19:10:23 +0300

    Проект 23.06

--- js/prize.js
+++ js/prize.js
@@ -3,24 +3,54 @@ const popup = document.querySelector('#prize-popup');
 const closeBtn = document.querySelector('#prize-popup__close');
 const nameField = document.querySelector('#prize-popup input[name="name"]').parentNode;
 const emailField = document.querySelector('#prize-popup input[name="email"]').parentNode;
+const form = document.getElementById('prize-form');
+const form = document.getElementById('prize-form');
 
 function popupToggle() {
     popup.classList.toggle('hidden')
 }
 
+const SELECT_SELECTED = 'input-select-selected';
 // const ERROR_CLASS_NAME =    
 
 function initializeField(field){
     const input = field.getElementsByTagName('input')[0];
     input.value = '';
 }
-initializeField(nameField);
-initializeField(emailField);
+return {
+    getValue() {
+        return input.value
+    },
+    focus() {
+        input.focus()
+    }
+}
 
-openBtn.onclick = popupToggle;
+const nameFieldUtils = initializeField(nameField);
+const emailFieldUtils = initializeField(emailField);
+
+openBtn.addEventListener('click', () => {
+    popupToggle();
+    nameFieldUtils.focus();
+});
+selectPrize.addEventListener('change', () => {
+    selectPrize.classList.add('input-select-selected')
+})
 closeBtn.onclick = popupToggle;
 
+function handleSubmit(event) {
+event.preventDefault();
+const data = {
+    name: nameFieldUtils.getValue(),
+    email: emailFieldUtils.getValue(),
+}
 
+const url = new URL('http://inno-ijl.ru/multystub/stc-21-03/feedback');
+url.search = new URLSearchParams(data).toString();
+
+fetch(url.toString());
+}
+form.addEventListener('submit', handleSubmit)
 
 //Занятие 23
 
